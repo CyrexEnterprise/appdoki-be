9e24a15db13dfbc1cf7dbcae1eb385f44037b521
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2020-12-02 17:27:56 +0000

    added transfer id to msg payload

--- app/users.go
+++ app/users.go
@@ -149,6 +149,7 @@ func (h *UsersHandler) GiveBeers(w http.ResponseWriter, r *http.Request) {
 			Body:  fmt.Sprintf("%s just rewarded %s with %d beers!", transfer.Giver.Name, transfer.Receiver.Name, beers),
 		}
 		data := map[string]string{
+			"id":    	string(transferID),
 			"giver":    string(giverJSON),
 			"receiver": string(receiverJSON),
 			"beers":    strconv.Itoa(beers),
